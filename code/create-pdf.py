1a7956f88d16d314d9ce1bd3dcadbfcefa342914
Oleksandra Voichenko <ol.voichenko@gmail.com>
2024-10-05 23:48:32 +0300

    add custom fonts

--- code/create-pdf.py
+++ code/create-pdf.py
@@ -1,4 +1,7 @@
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from settings import *
 
 class PdfCreator:
     def __init__(self, company_name, address, phone, description, work_interval, total):
@@ -8,6 +11,11 @@ class PdfCreator:
         self.service = description
         self.work_time = work_interval
         self.money = total
+
+        pdfmetrics.registerFont(TTFont('Captions', INVOICE_CAP_FONT))
+        pdfmetrics.registerFont(TTFont('Bold', BOLD_FONT))
+        pdfmetrics.registerFont(TTFont('Normal', NORMAL_FONT))
+
 
     def create(c):
         c.drawString(100, 100, "Hello World")
---
+++ code/settings.py
@@ -0,0 +1,5 @@
+from os.path import join
+
+INVOICE_CAP_FONT = join('..', 'fonts', 'Helvetica-Bold.ttf')
+BOLD_FONT = join('..', 'fonts', 'Helvetica.ttf')
+NORMAL_FONT = join('..', 'fonts', 'helvetica-light-587ebe5a59211.ttf')
