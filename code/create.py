bfc8e0d8eade66cab47c16c5145cca6eb51e1da1
Oleksandra Voichenko <ol.voichenko@gmail.com>
2024-10-06 00:34:47 +0300

    set up basic invoice template

---
+++ code/create.py
@@ -0,0 +1,41 @@
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.pagesizes import A4
+from settings import *
+
+class PdfCreator:
+    def __init__(self, personal_info, client_info, project_info):
+        self.personal_info = personal_info
+        self.client_info = client_info
+        self.project_info = project_info
+
+        self.width, self.height = A4
+
+        pdfmetrics.registerFont(TTFont('Captions', INVOICE_CAP_FONT))
+        pdfmetrics.registerFont(TTFont('Bold', BOLD_FONT))
+        pdfmetrics.registerFont(TTFont('Normal', NORMAL_FONT))
+
+
+    def create_base(self, c):
+        c.setFont('Captions', 40)
+        c.drawString(self.width - 220, self.height - 100, 'INVOICE')
+
+        # personal
+        c.setFont('Normal', 16)
+        y_pos = self.height - 100
+        for label, value in self.personal_info.items():
+            c.drawString(50, y_pos, value)
+            y_pos -= 22
+
+        # client
+        c.setFont('Normal', 16)
+        c.drawString(50, self.height - 220, 'Bill to:')
+        y_pos = self.height - 240
+        for label, value in self.personal_info.items():
+            c.drawString(50, y_pos, value)
+            y_pos -= 22
+
+
+    def fill(self, c):
+        pass
+
--- code/ui.py
+++ code/ui.py
@@ -1,11 +1,34 @@
-from flask import Flask
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello world!</p>"
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+from create import PdfCreator
+from reportlab.pdfgen import canvas
 
-app = Flask(__name__)
+personal_info = {'company name': '[company name]',
+                 'street address': '[street address]',
+                 'city, zip': '[city, zip]',
+                 'website': '[website]'}
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello world!</p>"
+client_info = {'client company': '[client company]',
+                'street address': '[street address]',
+                'city, zip': '[city, zip]'}
+
+project_info = {'work_interval': '[work_interval]',
+                'description': '[description]',
+                'payment': '[payment]'}
+
+c = canvas.Canvas("invoice.pdf")
+pdf_creator = PdfCreator(personal_info, client_info, project_info)
+pdf_creator.create_base(c)
+c.showPage()
+c.save()
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
